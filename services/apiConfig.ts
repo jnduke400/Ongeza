26b23d51cd973c73fb48669b66a1df8977c63f29
jnduke400 <ndukep@gmail.com>
2026-01-07 10:36:58 +0300

    -[fix], fixing endpoint

--- services/apiConfig.ts
+++ services/apiConfig.ts
@@ -1,3 +1,3 @@
 
 // Centralized configuration for API endpoints.
-export const API_BASE_URL = 'https://a185a9a774be.ngrok-free.app';
+export const API_BASE_URL = 'https://9d507ef4afc5.ngrok-free.app';
