6c9a236205c3a15a79582f62074cd3a556b4b3e4
jnduke400 <ndukep@gmail.com>
2026-01-19 16:18:46 +0300

    -change: cjanging endpoint

--- services/apiConfig.ts
+++ services/apiConfig.ts
@@ -1,2 +1,2 @@
 // Centralized configuration for API endpoints.
-export const API_BASE_URL = 'https://ae1f77fb302c.ngrok-free.app';
+export const API_BASE_URL = 'https://a46f452cdae5.ngrok-free.app';
