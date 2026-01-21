a6f50fcfdcbb0cd97d9ff819adb2a27fa399b5c4
jnduke400 <ndukep@gmail.com>
2026-01-21 10:35:57 +0300

    -change: changing endpoint

--- services/apiConfig.ts
+++ services/apiConfig.ts
@@ -1,2 +1,2 @@
 // Centralized configuration for API endpoints.
-export const API_BASE_URL = 'https://77f467a29d9f.ngrok-free.app';
+export const API_BASE_URL = 'https://c7bc215e960c.ngrok-free.app';
